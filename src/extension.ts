5a65eb460feae2fdec5fcacf4a7ceaad61e51d2a
Vinamra Sulgante <vsulgante@deloitte.com>
2023-08-02 10:41:53 +0530

    testing hook

--- src/extension.ts
+++ src/extension.ts
@@ -23,6 +23,7 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
+var name;
 
 
 // This method is called when your extension is deactivated
