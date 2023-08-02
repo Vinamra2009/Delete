e5968125e20f70797bee6a16df6aad70be9660e7
Vinamra Sulgante <vsulgante@deloitte.com>
2023-08-02 10:40:24 +0530

    testing hook

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+//Added a comment
 import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
@@ -21,6 +22,8 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(disposable);
 }
+
+
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
